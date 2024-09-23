51f2be4e0ddb94a6445a3577e12f047ce1e4a238
unknown <shaikusman88869@gmail.com>
2024-09-23 18:55:15 +0530

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import WorldLetterCounter from './component/WorldLetterCounter'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 id='top'>
+        Shaik Usman
+      </h1>
+      <h1>
+        Words and Letters Count
+      </h1>
+      <WorldLetterCounter />
+    </div>
+  );
+}
+
+export default App
---
+++ src/component/WorldLetterCounter.jsx
@@ -0,0 +1,38 @@
+
+
+import React, { useState } from 'react';
+import "./WorldLetterCounter.css";
+
+function WorldLetterCounter() {
+    const [text, setText] = useState("");
+    const wordCount = text.split(/\s+/).filter(Boolean).length;
+    const letterCount = text.length;
+
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    };
+  return (
+    <div>
+        <textarea 
+            placeholder='Type your text here...'
+            onChange={handleTextChange}
+            value={text}
+            rows={5}
+            cols={50}
+        />
+
+        <p>
+            Word Count:
+            {wordCount}
+        </p>
+
+        <p>
+            Letter Count:{" "}
+            {letterCount}
+        </p>
+    </div>
+  );
+}
+
+export default WorldLetterCounter;
+
